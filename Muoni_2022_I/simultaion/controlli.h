508a5c83e0a63ef522b7e3a5a04cd9db5d26e6c5
Giulia Cantarini <80638268+giiulia@users.noreply.github.com>
2022-12-13 11:53:06 +0100

    Update controlli.h

--- Muoni_2022_I/simultaion/controlli.h
+++ Muoni_2022_I/simultaion/controlli.h
@@ -5,4 +5,9 @@
 #define profondita 30 //in cm
 
 bool is_coincidence(double L, double K_x, double K_y, double x0, double y0);
+double rand_phi(double min, double max);
+double rand_theta(double valore_centrale);
+double rand_range(double min, double max);
+double get_path (double L, double K_x, double K_y, double x0, double y0);
+
 #endif
