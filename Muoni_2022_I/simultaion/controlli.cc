4b4ebfdbf6078eea49e7ccc691be575e3319767e
Giulia Cantarini <80638268+giiulia@users.noreply.github.com>
2022-12-13 11:52:21 +0100

    alttre funzioni utili aggiunte a controlli.cc

--- Muoni_2022_I/simultaion/controlli.cc
+++ Muoni_2022_I/simultaion/controlli.cc
@@ -11,3 +11,22 @@ bool is_coincidence(double L, double K_x, double K_y, double x0, double y0){ //L
     }
     else return false;
 }
+
+double rand_phi(double min, double max){
+    return min + (max - min) * rand() /static_cast<double> (RAND_MAX);
+}
+
+double rand_theta(double valore_centrale){
+    return 0.5*(valore_centrale + sin(valore_centrale)*cos(valore_centrale)) -0.5*(-0.5*M_PI+sin(-0.5*M_PI)*cos(-0.5*M_PI));
+}
+
+double rand_range(double min, double max){
+    return min + (max - min) *rand() /static_cast<double> (RAND_MAX);
+}
+
+double get_path (double L, double K_x, double K_y, double x0, double y0){
+    double delta_x = x0 - L*K_x;
+    double delta_y = y0 - L*K_y; 
+    double percorso = sqrt(pow(x0 - delta_x, 2) + pow(y0 - delta_y, 2) + pow(L, 2) );
+    return percorso;
+} 
