dbc16a03858ce43bfefe443d54a2157e165ef6ab
giiulia <80638268+giiulia@users.noreply.github.com>
2023-03-14 14:34:21 +0100

    grafici simulazione

--- Simulazione_traiettoria/simulazione.cpp
+++ Simulazione_traiettoria/simulazione.cpp
@@ -36,11 +36,13 @@ int main (int argc, char ** argv){
     srand(time(NULL)); 
     double path_lenght;
 
-    TH1F h_path("171.5 cm", "Simulazione spazio percorso muoni", sqrt(sqrt(N)), L - 1, 193);
-    TH2F h2_positions("stats", "Distribuzione sul secondo rivelatore", 160, -40, 40, 60, -15, 15); // nbinx minx maxx nbiny miny maxy
+    TH1F h_path("171.5 cm", " ", sqrt(sqrt(N)), L - 1, 89);
+    TH2F h2_positions("171.5 cm", " ", 160, -40, 40, 60, -15, 15); // nbinx minx maxx nbiny miny maxy
 
     int j=0;
     int conteggi = 0;
+    int conteggi_verticale = 0;
+
     while (j < N){
       muone muon(rand_theta(f_cos2, -M_PI/2, 0, 4/M_PI), rand_phi(0, 2*M_PI)); /*distribuzione cos^2*/
       double x0 = rand_range(-larghezza / 2, larghezza / 2);
@@ -52,6 +54,9 @@ int main (int argc, char ** argv){
         h2_positions.Fill(x_coinc_coord(L, muon.coeff_x(), x0), y_coinc_coord(L, muon.coeff_y(), y0));
   
         conteggi++;
+        if( controllo_verticale(muon.get_theta()) ){
+          conteggi_verticale ++;
+        }
 
       }
       if (j % 10000000 == 0){
@@ -63,6 +68,7 @@ int main (int argc, char ** argv){
 
     std::cout.precision(3);
     std::cout << "il " << conteggi*100./pow(10, 8) << " % " << " di " << N << " muoni sono passati sul secondo rilevatore dopo essere passati dal primo" << "\n" ;
+    std::cout << "il " << conteggi_verticale*100./pow(10, 8) << " % " << " di " << N << " muoni avevano inclinazione compresa in uno steradiante" << "\n" ;
 
     std::ofstream outfile_media("distanze.txt", std::ios::app);
     if (outfile_media.is_open())
@@ -71,49 +77,37 @@ int main (int argc, char ** argv){
       outfile_media.close();
     }
 
-    TH1D * projh2X = h2_positions.ProjectionX();
-    TH1D * projh2Y = h2_positions.ProjectionY();
+   // TH1D * projh2X = h2_positions.ProjectionX();
+   // TH1D * projh2Y = h2_positions.ProjectionY();
 
     gStyle->SetPalette(1);
 
     TCanvas c1;
-    h_path.GetXaxis()->SetTitle("distanza percorsa");
-    h_path.GetYaxis()->SetTitle("conteggi");
-    h_path.GetXaxis()->SetTitleSize(0.045);
-    h_path.GetYaxis()->SetTitleSize(0.045);
+    c1.SetLeftMargin(.15);
+    c1.SetBottomMargin(.15);
+
+
+    h_path.GetXaxis()->SetTitle(" distanza percorsa [cm]");
+    h_path.GetYaxis()->SetTitle(" conteggi ");
+    h_path.GetXaxis()->SetTitleSize(0.07);
+    h_path.GetYaxis()->SetTitleSize(0.07);
     h_path.SetFillColor (kOrange + 1) ;
     h_path.Draw ();
     std::string fileDistanze = "Distanze_simulate_";
     std::string lunghezza = argv[1];
     std::string estensione = ".pdf";
     c1.SetLogy();
-    c1.Print ((fileDistanze+lunghezza+estensione).c_str(), "pdf") ;
+   // c1.Print ((fileDistanze+lunghezza+estensione).c_str(), "pdf") ;
+   
 
     TCanvas c2;
+    c2.SetLeftMargin(.15);
+    c2.SetBottomMargin(.15);
     c2.SetRightMargin(.15);
     gStyle->SetPalette(57);
     h2_positions.Draw("COLZ1");
     std::string fileDistribuzione = "Distribuzione2D_";
     c2.Print((fileDistribuzione+lunghezza+estensione).c_str(), "pdf");
-    
-/*  a vedere come vengono i grafici non mi paiono cosi necessari come cosa aggiuntiva
-    TCanvas c3;
-    projh2X->SetFillColor(kAzure);
-    projh2X->Draw();
-    c3.Print("Distribuzione2Dx_171.5.pdf", "pdf");
-    TCanvas c4;
-    projh2Y->SetFillColor(kViolet);
-    projh2Y->Draw();
-    c4.Print("Distribuzione2Dy_171.5.pdf", "pdf");
-
-    questo a cosa serviva?
-    TCanvas c6;
-    h_theta.Draw();
-    c6.Print()
-    TCanvas c7;
-    h_phi.Draw();
-    c6.Print();
- */   
-    theApp.Run();
+    theApp.Run();    
     return 0;
   }
